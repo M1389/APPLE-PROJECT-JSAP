73c1816da121680d90c1cb39cc94dcd25890429d
Mahdi Hashemi <95425009+M1389@users.noreply.github.com>
2025-11-24 22:27:15 +0400

    add canvas

--- apple-jsap-project/src/Components/ProductViewer.jsx
+++ apple-jsap-project/src/Components/ProductViewer.jsx
@@ -1,30 +1,48 @@
 import React from "react";
+import useMacbookStore from "../store";
+import clsx from "clsx";
+import { Canvas } from "@react-three/fiber";
+import { Box } from "@react-three/drei";
 
 export default function ProductViewer() {
-  return (
+    const {color , scale , setColor , setScale} = useMacbookStore();
+   return (
     <section id="product-viewer">
       <h2>Tacke a closer look</h2>
 
       <div className="controls">
-        <p className="info">MacBook Pro 16" in Space Black</p>
+        <p className="info">MacBook Pro {scale} in {color}</p>
 
         <div className="flex-center gap-5 mt-5">
           <div className="color-control">
-            <div className="bg-neutral-300" />
-            <div className="bg-neutral-900" />
+            <div 
+                onClick={()=> setColor('#adb5bd')} 
+                className={clsx('bg-neutral-300', color ==  '#adb5bd' && 'active')} />
+            <div 
+                onClick={()=> setColor('#2e2c2e')} 
+                className={clsx('bg-neutral-900', color ==  '#2e2c2e' && 'active')} />
           </div>
+
           <div className="size-control">
-            <div>
-              <p>14"</p>
-            </div>
-            <div>
-              <p>16"</p>
-            </div>
+                <div 
+                    onClick={()=> setScale(0.06)} 
+                    className={clsx(scale == 0.06 ? 'bg-white text-black' : 'bg-transparent text-white' )} >
+                        <p>14"</p>
+                </div>
+
+                <div 
+                    onClick={()=> setScale(0.08)} 
+                    className={clsx(scale == 0.08 ? 'bg-white text-black' : 'bg-transparent text-white' )} >
+                        <p>16"</p>
+                </div>
           </div>
         </div>
       </div>
 
-      <p>Render Canvas</p>
+      <Canvas id="canvas">
+        <Box position={[-1,1,0]} scale={10 * scale} material-color={color} />
+
+      </Canvas>
     </section>
   );
 }
