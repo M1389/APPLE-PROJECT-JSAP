62f81390fe9cec476d03c7f198161c7d9a5c8ea8
Mahdi Hashemi <95425009+M1389@users.noreply.github.com>
2025-12-04 00:26:47 +0400

    Start performance section and update constance file

---
+++ apple-jsap-project/src/Components/Performance.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Performance() {
+  return (
+    <section id='performance'>
+        <h2>Next level graphic performace. Game On</h2>
+
+        <div className='wrapper'>
+
+        </div>
+    </section>
+  )
+}
